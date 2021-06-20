e1d86c30fdcdf06942c19a8468e0349d343f045c
Usichenko Vlad <usichenko.vlad67@gmail.com>
2021-06-20 13:36:17 +0300

    Comments

--- dir.py
+++ dir.py
@@ -1,29 +1,47 @@
 import os
+from colorama import Fore
 
 
-def makepath(path: str):
+def makepath(in_path: str):
+    """
+    Создние директорий используя строку содежащую полный путь каталогов
+    :param in_path: Строка полного пути каталогов
+    :return: True если указанные каталоги были созданы |
+    False в случае неудачи
+    """
     try:
-        path = os.path.normpath(path)
-        path = path.split(os.sep)
-        if len(path) <= 1:
-            print('Путь к файлу указан некорректно')
-            return -1
-        if path[0] == '':
-            path.pop(0)
+        in_path = os.path.normpath(in_path)
+        in_path = in_path.split(os.sep)
+        if len(in_path) <= 1:
+            print(Fore.RED + 'Путь к файлу указан некорректно')
+            return False
+        if in_path[0] == '':
+            in_path.pop(0)
+        layers = len(in_path)
     except TypeError:
-        print('Укажите корректный путь в строковом формате')
-        return -1
+        print(Fore.RED + 'Укажите корректный путь в строковом формате')
+        return False
 
-    parent = path.pop(0) + "\\"
-    while len(path) != 0:
+    parent = in_path.pop(0) + "\\"
+    depth = 0
+
+    while len(in_path) != 0:
         try:
-            new_dir = path.pop(0)
+            new_dir = in_path.pop(0)
             parent = os.path.join(parent, new_dir)
             os.mkdir(parent, 0o777)
+            print(Fore.GREEN + f"Директория {new_dir} была создана в {parent}")
         except os.error as err:
             if err.errno == 17:
-                print(f"Такой файл уже существует {err.filename}")
+                print(Fore.YELLOW + f"Такая директория уже существует {err.filename}")
+                depth += 1
                 continue
-            print(err)
-            return -1
-    return 1
+            print(Fore.RED + err)
+            return False
+    if depth == (layers - 1):
+        return False
+    return True
+
+
+path = fr"{input('Введите путь к директории:')}"
+print(Fore.GREEN + "Путь был успешно создан") if makepath(path) else print(Fore.RED + "Ошибка при создании директорий")
